fe8a73f763db93f93febd8d82d3eb93460eca0ab
Michael Noser <michael@noser.moe>
2020-04-22 09:43:26 +0200

    tweak some values

--- shaders/particle_physics.comp.glsl
+++ shaders/particle_physics.comp.glsl
@@ -17,12 +17,18 @@ layout(set = 1, binding = 0) uniform UniformBufferObject {
 } ubo;
 
 void main() {
-    float gravity = 1.0;
+    float gravity = 0.1;
+    float max_vel = 2.0;
 
     uint idx = gl_GlobalInvocationID.x;
     vec2 delta = normalize(ubo.target - vertices.data[idx].position) * gravity;
 
     vertices.data[idx].velocity += delta;
+
+    if (length(vertices.data[idx].velocity) > max_vel) {
+        vertices.data[idx].velocity /= length(vertices.data[idx].velocity);
+        vertices.data[idx].velocity *= max_vel;
+    }
 
     vertices.data[idx].position += vertices.data[idx].velocity * ubo.delta_time;
 }
